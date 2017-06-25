737a1867c9470d500821151e4b0d466654b10b6d
plarus <plarus@users.noreply.github.com>
2017-06-25 19:28:38 +0200

    Remove unneeded util function

--- src/c/util.c
+++ src/c/util.c
@@ -27,36 +27,3 @@ int16_t get_obstruction_height(Layer *s_window_layer) {
     return fullscreen.size.h - unobstructed_bounds.size.h;
 }
 
-#ifdef PBL_HEALTH
-   bool is_health_metric_accessible(HealthMetric metric) {
-     time_t start = time_start_of_today();
-     time_t end = time(NULL);
-
-     // Check step data is available
-     HealthServiceAccessibilityMask mask = health_service_metric_accessible(metric, start, end);
-     bool result = mask & HealthServiceAccessibilityMaskAvailable;
-
-     return result;
-   }
-
-   bool is_user_sleeping() {
-     // temporarily disable sleep function until Pebble gets their act together
-     return false;
-     //
-    //  time_t now = time(NULL);
-     //
-    //  HealthActivityMask activities = HealthActivitySleep | HealthActivityRestfulSleep;
-     //
-    //  HealthServiceAccessibilityMask mask = health_service_any_activity_accessible(activities, now, now);
-    //  bool sleep_access_available = mask & HealthServiceAccessibilityMaskAvailable;
-     //
-    //  if(sleep_access_available) {
-    //    uint32_t current_activities = health_service_peek_current_activities();
-    //    bool sleeping = current_activities & HealthActivitySleep || current_activities & HealthActivityRestfulSleep;
-     //
-    //    return sleeping;
-    //  } else {
-    //    return false;
-    //  }
-   }
- #endif
